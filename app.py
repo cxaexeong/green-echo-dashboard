3511a35b752adf86da6b41988a329bf1b34500bc
Chaeyeong Lee <130729886+cxaexeong@users.noreply.github.com>
2026-06-24 18:24:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -207,6 +207,19 @@ if search_name:
             .reset_index()
             .sort_values("AI_유사기관군")
         )
+        st.write("현재 분석 대상 기관유형:", SELECTED_TYPE)
+
+        st.dataframe(
+            pattern_stats[
+                [
+                    "AI_유사기관군",
+                    "기관수",
+                    "평균_구매비율",
+                    "평균_총구매액",
+                    "평균_구매비율_변화량",
+                ]
+            ]
+        )
 
         def rank_text(rank, total):
             if rank == 1: return "가장 높은"
